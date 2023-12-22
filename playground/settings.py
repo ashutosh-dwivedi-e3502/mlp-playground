deae9c778231c79ecf0184d4b1739578f141ceeb
Ashutosh Dwivedi <ashutosh.dwivedi@freshworks.com>
2023-12-22 07:19:15 +0000

    added vit dataloader

--- playground/settings.py
+++ playground/settings.py
@@ -1,3 +1,7 @@
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+DATA_DIR = os.path.join(BASE_DIR, 'data')
+
+MODELS_DIR = os.path.join(BASE_DIR, 'models')
---
+++ playground/vit/dataloader.py
@@ -0,0 +1,42 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import settings
+
+image_size = (32, 32, 3)
+height, width, channels = image_size
+
+def get_dataloader(train:bool, transform:nn.Module, batch_size):
+    dataset = torchvision.datasets.CIFAR10(
+        root=settings.DATA_DIR,        
+        train=False,
+        download=True,
+        transform=transform,
+    )
+    trainloader = torch.utils.data.DataLoader(
+        dataset, batch_size=batch_size, shuffle=True, drop_last=True
+    )
+
+def get_test_dataloader(batch_size):
+    transform = transforms.Compose(
+        [
+            transforms.Resize((height, width)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ]
+    )
+    return get_dataloader(train=False, transform=transform, batch_size=batch_size)
+
+
+def get_train_dataloader(batch_size):
+    transform = transforms.Compose(
+        [
+            transforms.RandomCrop(32, padding=4),
+            transforms.Resize((height, width)),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ]
+    )
+    return get_dataloader(train=True, transform=transform, batch_size=batch_size)
