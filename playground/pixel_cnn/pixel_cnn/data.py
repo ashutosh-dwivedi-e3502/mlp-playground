330d17167ab7bb6f77f51c4f558b938af8a68b3a
Ashutosh Dwivedi <ashutosh.dwivedi@freshworks.com>
2024-12-08 15:14:55 +0000

    fixed dataloading function

--- playground/pixel_cnn/pixel_cnn/data.py
+++ playground/pixel_cnn/pixel_cnn/data.py
@@ -1,5 +1,11 @@
-from datasets import load_dataset
+from datasets import load_dataset, Dataset
+from jaxtyping import Float, Array
+from typing import Tuple
 
-def get_dataset():
-    datset = load_dataset("minist")
-    return dataset
+def get_dataset(device: str) -> Dataset:
+    mnist = load_dataset("ylecun/mnist")
+    # only select images because we don't need labels for this task
+    mnist = mnist.with_format(type="jax", columns=["image"], device=device)
+    # make the 28 X 28 image a 1 X 28 X 28 image
+    mnist = mnist.map(lambda x: {"image": x["image"].expand_dims(axis=0)}, batched=True)
+    return mnist
