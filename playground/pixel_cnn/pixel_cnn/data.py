fa88dcf7a63cef36d5be45ab325497b7ce040550
Ashutosh Dwivedi <ashutosh.dwivedi@freshworks.com>
2024-12-08 15:16:11 +0000

    added comment for channel

--- playground/pixel_cnn/pixel_cnn/data.py
+++ playground/pixel_cnn/pixel_cnn/data.py
@@ -6,6 +6,6 @@ def get_dataset(device: str) -> Dataset:
     mnist = load_dataset("ylecun/mnist")
     # only select images because we don't need labels for this task
     mnist = mnist.with_format(type="jax", columns=["image"], device=device)
-    # make the 28 X 28 image a 1 X 28 X 28 image
+    # make the 28 X 28 image a 1 X 28 X 28 image (add channel dimension)
     mnist = mnist.map(lambda x: {"image": x["image"].expand_dims(axis=0)}, batched=True)
     return mnist
